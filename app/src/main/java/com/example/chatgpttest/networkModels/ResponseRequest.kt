6243fb75eb5c67ce93bc0b770ef65f659855d9c1
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-05-14 14:33:41 -0400

    Update ResponseRequest.kt

--- app/src/main/java/com/example/chatgpttest/networkModels/ResponseRequest.kt
+++ app/src/main/java/com/example/chatgpttest/networkModels/ResponseRequest.kt
@@ -1,10 +1,13 @@
 package com.example.chatgpttest.networkModels
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ResponseRequest(
     val model: String,
     val input: String,
-    val stream: Boolean
+    val stream: Boolean,
+    @SerialName("previous_response_id")
+    val previousResponseId: String?
 )
